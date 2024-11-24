357a63f983c8b1bf84f478912c94a7415f697ecb
SvetaSaletska <svitlanagalevich@gmail.com>
2024-11-25 00:46:25 +0200

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,14 +1,12 @@
-var swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper('.mySwiper', {
+  loop: true, // Дозволяє перехід з останнього слайду на перший
   slidesPerView: 'auto', // Автоматичне налаштування ширини слайдів
-  spaceBetween: 10,
-  cssMode: true,
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
+  spaceBetween: 10, // Відстань між слайдами
+  centeredSlides: false, // Не центрує слайди, щоб частина наступного була видна справа
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination', // Елемент пагінації
+    clickable: true, // Дозволяє кліки по крапочках пагінації
   },
-  mousewheel: true,
-  keyboard: true,
+  mousewheel: true, // Дозволяє прокручувати слайди колесом миші
+  keyboard: true, // Дозволяє перемикати слайди за допомогою клавіатури
 });
